f911d06e57f0dfa108f9268c41892beeed6e11fb
Adali Carvajal <77343598+adalicarvajal@users.noreply.github.com>
2025-01-27 16:42:04 -0500

    Add files via upload

---
+++ micro-participante/src/main/java/com/espe/micro_participante/MicroParticipanteApplication.java
@@ -0,0 +1,13 @@
+package com.espe.micro_participante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroParticipanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroParticipanteApplication.class, args);
+	}
+
+}
---
+++ micro-participante/src/main/java/com/espe/micro_participante/model/entity/Participante.java
@@ -0,0 +1,39 @@
+package com.espe.micro_participante.model.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Participante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String nombre;
+    private String email;
+
+    // Getters y Setters
+}
