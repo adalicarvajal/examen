2f2d6e67cbeb52d668cf86a2a564191443fe6aa7
Adali Carvajal <77343598+adalicarvajal@users.noreply.github.com>
2025-01-27 16:41:23 -0500

    Delete micro-taller directory

--- micro-taller/src/main/java/com/espe/micro_taller/repositories/TallerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.espe.micro_taller.repositories;
-
-import com.espe.micro_taller.model.entity.Taller;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface TallerRepository extends JpaRepository<Taller, Long> {
-    Optional<Taller> findById(Long id);
-}
