b098dc2b6730895164beb0b6d3e5c5604c8dceb5
Adali Carvajal <77343598+adalicarvajal@users.noreply.github.com>
2025-01-27 16:41:08 -0500

    Delete micro-participante directory

--- micro-participante/src/main/java/com/espe/micro_participante/repositories/ParticipanteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.espe.micro_participante.repositories;
-
-import com.espe.micro_participante.model.entity.Participante;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ParticipanteRepository extends JpaRepository<Participante, Long> {
-}
